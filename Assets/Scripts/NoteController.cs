c952a55e321e5b073ac95b77bf618a0404aec6bd
Skymaslo <wsop.top.chips@mail.ru>
2024-03-15 17:36:30 +0300

    house

--- Assets/Scripts/NoteController.cs
+++ Assets/Scripts/NoteController.cs
@@ -2,18 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NoteController : MonoBehaviour
 {
+    public GameObject note;
     bool inTrigger = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     private void ShowNote()
     {
-        
+        note.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void CloseNote()
+    {
+        note.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -21,7 +33,26 @@ public class NoteController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F) && inTrigger == true)
         {
-            
+            ShowNote();
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.tag == "Player")
+        {
+            inTrigger = true;
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag != "Player")
+        {
+            inTrigger = false;
+        }
+
     }
 }
